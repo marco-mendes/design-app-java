5fab5c9eeb8e18d6195bbab210deb91e19274bb8
GladsonBruno <gladsonbruno16@gmail.com>
2019-06-24 17:13:31 -0300

    Finalizando laboratório 8

--- Programacao Funcional/src/laboratorio8/exercicio/IFBinaryOperatorExercicio.java
+++ Programacao Funcional/src/laboratorio8/exercicio/IFBinaryOperatorExercicio.java
@@ -6,8 +6,13 @@ public class IFBinaryOperatorExercicio {
 
     public static void main(String[] args) {
 
-        BinaryOperator<Double> obtemRaizQuadradaDaSubtracao = (value1, value2) -> Math.sqrt(value1 - value2);
+        // Remova os comentários para iniciar o exercício
+        /*
+
+        BinaryOperator<Double> obtemRaizQuadradaDaSubtracao = ?????;
         System.out.println(obtemRaizQuadradaDaSubtracao.apply(70.0, 6.0));
+
+        */
 
     }
 
