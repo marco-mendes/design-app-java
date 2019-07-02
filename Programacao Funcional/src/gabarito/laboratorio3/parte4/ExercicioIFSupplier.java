a2f6b5ccb838ad95f34139adec48f6e905dbdff8
GladsonBruno <gladsonbruno16@gmail.com>
2019-07-02 11:05:21 -0300

    Adicionando Laboratorio 3 - Parte 4 ao Gabarito

---
+++ Programacao Funcional/src/gabarito/laboratorio3/parte4/ExercicioIFSupplier.java
@@ -0,0 +1,14 @@
+package gabarito.laboratorio3.parte4;
+
+import java.util.function.Supplier;
+
+public class ExercicioIFSupplier {
+
+    public static void main(String[] args) {
+        // Resolvido
+        Supplier<Double> valorAleatorio = () -> Math.random();
+        System.out.println(valorAleatorio.get());
+
+    }
+
+}
