ebf0967e8b258c46e311ab290c80b4997b01a209
GladsonBruno <gladsonbruno16@gmail.com>
2019-07-02 10:47:58 -0300

    Adicionando Laboratorio 3 - Parte 3 ao Gabarito

---
+++ Programacao Funcional/src/gabarito/laboratorio3/parte3/ExercicioIFConsumer.java
@@ -0,0 +1,38 @@
+package gabarito.laboratorio3.parte3;
+
+import java.util.function.Consumer;
+
+public class ExercicioIFConsumer {
+
+    public static void main(String[] args) {
+
+        //Resolvido
+        Consumer<Pessoa> consumerImprimeNome = p -> System.out.print("Nome: " + p.getNome() + " - ");
+        Consumer<Pessoa> consumerImprimeNomeEIdade = consumerImprimeNome.andThen(p -> System.out.print("Idade: " + p.getIdade()));
+
+        Pessoa p = new Pessoa("João", 32);
+
+        consumerImprimeNomeEIdade.accept(p);
+    }
+
+}
+
+class Pessoa {
+
+    String nome;
+    int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+}
