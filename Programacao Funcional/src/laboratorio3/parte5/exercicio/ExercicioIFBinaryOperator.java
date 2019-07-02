f5071de51059c71db5749c8c711c91b19c0cd698
GladsonBruno <gladsonbruno16@gmail.com>
2019-07-02 11:31:54 -0300

    Iniciando Laboratorio 3 - Parte 5

---
+++ Programacao Funcional/src/laboratorio3/parte5/exercicio/ExercicioIFBinaryOperator.java
@@ -0,0 +1,17 @@
+package laboratorio3.parte5.exercicio;
+
+public class ExercicioIFBinaryOperator {
+
+    public static void main(String[] args) {
+
+        // Remova os comentários para iniciar o exercício
+        /*
+
+        BinaryOperator<Double> obtemRaizQuadradaDaSubtracao = ?????;
+        System.out.println(obtemRaizQuadradaDaSubtracao.apply(70.0, 6.0));
+
+        */
+
+    }
+
+}
---
+++ Programacao Funcional/src/laboratorio3/parte5/exercicio/ExercicioIFUnaryOperator.java
@@ -0,0 +1,15 @@
+package laboratorio3.parte5.exercicio;
+
+public class ExercicioIFUnaryOperator {
+    public static void main(String[] args) {
+
+        // Remova os comentários para iniciar o exercício
+        /*
+
+        UnaryOperator<Double> aplicaImposto = ????;
+        Double valorMercadoria = 2500.00;
+        System.out.println("Valor total mercadoria com imposto: " + aplicaImposto.apply(valorMercadoria));
+
+        */
+    }
+}
