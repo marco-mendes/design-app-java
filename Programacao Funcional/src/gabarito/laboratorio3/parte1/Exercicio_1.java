ffbb9123fab97208eb000afc274963e80f6d00b5
GladsonBruno <gladsonbruno16@gmail.com>
2019-07-08 15:05:24 -0300

    Reformulando Gabarito 3 - Parte 1

---
+++ Programacao Funcional/src/gabarito/laboratorio3/parte1/Exercicio_1.java
@@ -0,0 +1,14 @@
+package gabarito.laboratorio3.parte1;
+
+import java.util.function.Function;
+
+public class Exercicio_1 {
+
+    public static void main(String[] args) {
+
+        Function<String, String> helloPeople = (nome) -> "Hello " + nome;
+        System.out.println(helloPeople.apply("Jhon"));
+
+    }
+
+}
---
+++ Programacao Funcional/src/gabarito/laboratorio3/parte1/Exercicio_5.java
@@ -0,0 +1,17 @@
+package gabarito.laboratorio3.parte1;
+
+import java.util.function.Function;
+
+public class Exercicio_5 {
+
+    public static Double calcular (Double valor, Function<Double, Double> operation){
+        return operation.apply(valor);
+    }
+
+    public static void main(String[] args) {
+        Function<Double, Double> raizQuadrada = (value) -> Math.sqrt(value);
+        System.out.println(calcular(25.0, raizQuadrada));
+
+    }
+
+}
