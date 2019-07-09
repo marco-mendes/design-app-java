e2fdee57a64af6d9ea7906d302e6d1c0f1d34b81
GladsonBruno <gladsonbruno16@gmail.com>
2019-07-09 12:00:22 -0300

    Adicionado Gabarito - Laboratorio 3 - Parte 5

---
+++ Programacao Funcional/src/gabarito/laboratorio3/parte5/Exercicio_2.java
@@ -0,0 +1,14 @@
+package gabarito.laboratorio3.parte5;
+
+import java.util.function.BinaryOperator;
+
+public class Exercicio_2 {
+
+    public static void main(String[] args) {
+
+        BinaryOperator<Double> obtemRaizQuadradaDaSubtracao = (value1, value2) -> Math.sqrt(value1 - value2);
+        System.out.println(obtemRaizQuadradaDaSubtracao.apply(70.0, 6.0));
+
+    }
+
+}
