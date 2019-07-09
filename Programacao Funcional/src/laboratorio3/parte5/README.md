414ef269116aa74dfe4d55ca2a000dbe4bfbf1b6
GladsonBruno <gladsonbruno16@gmail.com>
2019-07-09 12:20:04 -0300

    Adicionando exercicio 3 ao Laboratorio 3 - Parte 5

--- Programacao Funcional/src/laboratorio3/parte5/README.md
+++ Programacao Funcional/src/laboratorio3/parte5/README.md
@@ -152,3 +152,55 @@ public class ExemploIFBinaryOperator {
     
 }
 ```
+
+#### Exercício 3
+Com base no código abaixo siga os seguintes passos:
+ * Crie um BinaryOperator chamado **produtoMaisBarato**, o mesmo deve verificar qual dos dois produtos é o mais barato.<br/>
+ * Crie um BinaryOperator chamado **produtoMaisCaro**, o mesmo deve verificar qual dos dois produtos é o mais caro.<br/>
+ * Utilize os métodos estáticos **maxBy** e **minBy** para criar esses 2 BinaryOperator.
+ * Invoque cada um dos BinaryOperator utilizando os 2 objetos do tipo produto já criados e imprima no console o resultado de cada invocação.
+ 
+```java
+import java.util.function.BinaryOperator;
+
+public class Exercicio_3 {
+
+    public static void main(String[] args) {
+
+        Produto produto1 = new Produto("Celular Galaxy J5 Prime", 899.00);
+        Produto protudo2 = new Produto("Xiaomi Mi 8", 1999.00);
+        
+        BinaryOperator<Produto> produtoMaisBarato = ?????;
+        BinaryOperator<Produto> produtoMaisCaro = ?????;
+       
+    }
+
+}
+
+class Produto {
+
+    String nome;
+    Double preco;
+
+    public Produto(String nome, Double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
+```
---
+++ Programacao Funcional/src/laboratorio3/parte5/exercicio/Exercicio_3.java
@@ -0,0 +1,57 @@
+package laboratorio3.parte5.exercicio;
+
+import java.util.function.BinaryOperator;
+
+/*
+Com base no código abaixo siga os seguintes passos:
+ * Crie um BinaryOperator chamado produtoMaisBarato, o mesmo deve verificar qual dos dois produtos é o mais barato.<br/>
+ * Crie um BinaryOperator chamado produtoMaisCaro, o mesmo deve verificar qual dos dois produtos é o mais caro.<br/>
+ * Utilize os métodos estáticos maxBy e minBy para criar esses 2 BinaryOperator.
+ * Invoque cada um dos BinaryOperator utilizando os 2 objetos do tipo produto já criados e imprima no console o resultado.
+* */
+
+public class Exercicio_3 {
+
+    public static void main(String[] args) {
+
+        // Remova os comentários abaixo para iniciar o exercício!
+        /*
+
+        Produto produto1 = new Produto("Celular Galaxy J5 Prime", 899.00);
+        Produto protudo2 = new Produto("Xiaomi Mi 8", 1999.00);
+
+        BinaryOperator<Produto> produtoMaisBarato = ?????;
+        BinaryOperator<Produto> produtoMaisCaro = ?????;
+
+        */
+
+    }
+
+}
+
+class Produto {
+
+    String nome;
+    Double preco;
+
+    public Produto(String nome, Double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
