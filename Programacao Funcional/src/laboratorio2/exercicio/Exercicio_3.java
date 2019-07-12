71d5f7467b8232ede772ed99443947b8b5d3f533
GladsonBruno <gladsonbruno16@gmail.com>
2019-07-12 10:38:25 -0300

    Revisao nova reestruturacao lab 2

--- Programacao Funcional/src/laboratorio2/exercicio/Exercicio_3.java
+++ Programacao Funcional/src/laboratorio2/exercicio/Exercicio_3.java
@@ -21,8 +21,7 @@ public class Exercicio_3 {
     }
 
     public static void main(String[] args) {
-        //Produto produto = buscarProdutoPorId("5");
-        Optional<Produto> produto = Optional.ofNullable(buscarProdutoPorId("5"));
+        Produto produto = buscarProdutoPorId("5");
     }
 
 }
