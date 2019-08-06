1fcc4e14437a2a43a98fcc3ee499ed2259cb9ef7
GladsonBruno <gladsonbruno16@gmail.com>
2019-08-06 10:45:44 -0300

    Adicionando Lab 1 - Java 9 - Parte 3 - Exercicio 1

---
+++ Novidades Java/src/gabarito/laboratorio1/parte3/exercicio1/TweetSubscriber.java
@@ -0,0 +1,40 @@
+package gabarito.laboratorio1.parte3.exercicio1;
+
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+
+public class TweetSubscriber implements Subscriber<Tweet> {
+
+    private Subscription subscription;
+
+    private int counter = 0;
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        System.out.println("Verificando o que há de novo!");
+        this.subscription = subscription;
+        this.subscription.request(1);
+    }
+
+    @Override
+    public void onNext(Tweet tweet) {
+        System.out.println(String.format("O usuário %s acabou de Tweetar: %s", tweet.getUsuario(), tweet.getTextoTweet()));
+        counter++;
+        this.subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        e.printStackTrace();
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println("Isso é tudo por hoje!");
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+}
