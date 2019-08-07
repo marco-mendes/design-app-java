40553968c0d22bf36c761cffb136e667af3446e8
GladsonBruno <gladsonbruno16@gmail.com>
2019-08-07 16:28:53 -0300

    Laboratorio 1 - Java 9 - Parte 5 Adicionando Gabarito

--- Novidades Java/gabarito/laboratorio1/README.md
+++ Novidades Java/gabarito/laboratorio1/README.md
@@ -4,6 +4,4 @@
 ### [Parte 2](./parte2)
 ### [Parte 3](./parte3)
 ### [Parte 4](./parte4)
-<!--
-### [Parte 5]()
--->
+### [Parte 5](./parte5)
---
+++ Novidades Java/gabarito/laboratorio1/parte5/exercicio/src/com/exercicio/Exercicio_1.java
@@ -0,0 +1,52 @@
+package com.exercicio;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Exercicio_1 {
+
+    public static void main(String[] args) {
+        Optional<Carro> carro1 = Carro.buscarCarroPorModelo("Palio");
+        Optional<Carro> carro2 = Carro.buscarCarroPorModelo("Siena");
+
+        carro1.ifPresentOrElse(c -> c.imprimeCarro(), () -> System.out.println("Modelo não cadastrado"));
+        carro2.ifPresentOrElse(c -> c.imprimeCarro(), () -> System.out.println("Modelo não cadastrado"));
+
+    }
+
+}
+
+class Carro {
+    private String modelo;
+    private String marca;
+
+    public Carro(String modelo, String marca) {
+        this.modelo = modelo;
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public static Optional<Carro> buscarCarroPorModelo(String modelo) {
+        List<Carro> listaCarros = Arrays.asList(
+                new Carro("Palio", "Fiat"),
+                new Carro("Gol", "Volkswagen"),
+                new Carro("Versa", "Nissan")
+        );
+
+        Optional<Carro> resultado = listaCarros.stream().filter(carro -> carro.getModelo().equals(modelo)).findFirst();
+        return resultado;
+    }
+
+    public void imprimeCarro() {
+        System.out.println(String.format("Carro modelo: %s, Marca: %s", this.modelo, this.marca));
+    }
+
+}
