ceb343d55dcd6ed858bfd77c59a5a0658a6b618d
GladsonBruno <gladsonbruno16@gmail.com>
2019-08-07 14:56:49 -0300

    Laboratorio 1 - Java 9 - Parte 4 adicionando gabarito

--- Novidades Java/gabarito/laboratorio1/README.md
+++ Novidades Java/gabarito/laboratorio1/README.md
@@ -3,7 +3,7 @@
 ### [Parte 1](./parte1)
 ### [Parte 2](./parte2)
 ### [Parte 3](./parte3)
+### [Parte 4](./parte4)
 <!--
-### [Parte 4]()
 ### [Parte 5]()
 -->
---
+++ Novidades Java/gabarito/laboratorio1/parte4/exercicio2/src/com/exercicio2/Exercicio_2.java
@@ -0,0 +1,19 @@
+package com.exercicio2;
+
+/*
+Crie 3 coleções imutáveis utilizando o método of das interfaces List, Set e Map para uma coleção de 5 números inteiros.
+* */
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Exercicio_2 {
+
+    public static void main(String[] args) {
+        List<Integer> listaImutavel = List.of(1,2,3,4,5);
+        Set<Integer> setImutavel = Set.of(1,2,3,4,5);
+        Map<String, Integer> mapImutavel = Map.of("Chave 1", 1, "Chave 2", 2, "Chave 3", 3, "Chave 4", 4, "Chave 5", 5);
+    }
+
+}
