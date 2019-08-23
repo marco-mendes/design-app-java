d52ac71f2e400b94020106e2a21c23ebe3ffaf5c
GladsonBruno <gladsonbruno16@gmail.com>
2019-08-23 17:34:08 -0300

    Modulo 2 - Atualizando exercicio 1 - Laboratorio 1

--- NovosLaboratorios/src/gabarito/laboratorio1/exercicio1/PetShop.java
+++
@@ -1,9 +0,0 @@
-package gabarito.laboratorio1.exercicio1;
-
-public class PetShop {
-
-    public void tosarCachorro(Cachorro cachorro) {
-        System.out.println(String.format("Tosando: %s | Raça: %s", cachorro.getNome(), cachorro.getRaca()));
-    }
-
-}
---
+++ NovosLaboratorios/src/gabarito/laboratorio1/exercicio1/Usuario.java
@@ -0,0 +1,29 @@
+package gabarito.laboratorio1.exercicio1;
+
+import java.util.List;
+
+public class Usuario {
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+}
