3d509506b108c73ec5d9afbab819496d25984093
GladsonBruno <gladsonbruno16@gmail.com>
2019-09-03 02:37:33 -0300

    Renomeando pasta dos novos laboratorios

---
+++ API Java SE/src/lab5/Exercicio1.java
@@ -0,0 +1,23 @@
+package lab5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        List<String> valores = new ArrayList<>();
+        valores.addAll(Arrays.asList("Primeiro valor", "Segundo valor", "Terceiro valor", "Quarto valor", "Ultimo valor"));
+
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.addAll(valores);
+        linkedList.removeFirst();
+        linkedList.removeLast();
+        linkedList.addFirst("Novo primeiro elemento");
+        linkedList.addLast("Novo ultimo elemento");
+        System.out.println(linkedList);
+    }
+
+}
---
+++ API Java SE/src/lab5/Exercicio3.java
@@ -0,0 +1,17 @@
+package lab5;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+        List<Integer> listaInteiros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        Stack<Integer> stack = new Stack<>();
+        stack.addAll(listaInteiros);
+        stack.removeIf(v -> v % 2 != 0);
+        System.out.println(stack.peek());
+    }
+
+}
