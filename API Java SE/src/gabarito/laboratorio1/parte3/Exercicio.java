3cc5528598fe2bf46136ab201b2c1b6822863e06
GladsonBruno <gladsonbruno16@gmail.com>
2019-09-04 15:31:35 -0300

    Melhoria resolucao Exercicio 3

--- API Java SE/src/gabarito/laboratorio1/parte3/Exercicio.java
+++ API Java SE/src/gabarito/laboratorio1/parte3/Exercicio.java
@@ -4,12 +4,26 @@ public class Exercicio {
 
     public static void main(String[] args) {
         String texto = "A mulher é muito bonita";
-        Expression textInterpreter = new TextInterpreter(texto, TextUtils.obterAdjetivos(), TextUtils.obterArtigos(), TextUtils.obterSubstantivos());
-        textInterpreter.interpret();
+        Expression localizarArtigoInterpreter = new LocalizarArtigoInterpreter(texto, TextUtils.obterArtigos());
+        localizarArtigoInterpreter.interpret();
+
+        Expression localizarAdjetivoInterpreter = new LocalizarAdjetivoInterpreter(texto, TextUtils.obterAdjetivos());
+        localizarAdjetivoInterpreter.interpret();
+
+        Expression localizarSubstantivoInterpreter = new LocalizarSubstantivoInterpreter(texto, TextUtils.obterSubstantivos());
+        localizarSubstantivoInterpreter.interpret();
+
 
         String texto1 = "O João é um homem brasileiro";
-        Expression textInterpreter1 = new TextInterpreter(texto1, TextUtils.obterAdjetivos(), TextUtils.obterArtigos(), TextUtils.obterSubstantivos());
-        textInterpreter1.interpret();
+        Expression localizarArtigoInterpreter1 = new LocalizarArtigoInterpreter(texto1, TextUtils.obterArtigos());
+        localizarArtigoInterpreter1.interpret();
+
+        Expression localizarAdjetivoInterpreter1 = new LocalizarAdjetivoInterpreter(texto1, TextUtils.obterAdjetivos());
+        localizarAdjetivoInterpreter1.interpret();
+
+        Expression localizarSubstantivoInterpreter1 = new LocalizarSubstantivoInterpreter(texto1, TextUtils.obterSubstantivos());
+        localizarSubstantivoInterpreter1.interpret();
+
 
     }
 
