d707523ae91b25cb8f4b4ac14836a36337a1bcda
GladsonBruno <gladsonbruno16@gmail.com>
2019-09-11 10:54:58 -0300

    Commit laboratorio SpringBoot

---
+++ Introducao SpringBoot/exercicio/Pessoa.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Pessoa {
+
+
+    private int id;
+    private String nome;
+
+    public Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return id == pessoa.id &&
+                Objects.equals(nome, pessoa.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+}
