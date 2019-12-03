0d3f3a24bededdde1f055c84046c5d995593ff98
GladsonBruno <gladsonbruno16@gmail.com>
2019-12-03 15:52:55 -0300

    Update

--- Testes com Java/PiramideDeTestes/codigo/spring-testing/src/test/java/example/ExampleProviderTest.java
+++ Testes com Java/PiramideDeTestes/codigo/spring-testing/src/test/java/example/ExampleProviderTest.java
@@ -1,14 +1,7 @@
 package example;
 
-import static org.mockito.Mockito.when;
-
-import java.util.Optional;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.RestPactRunner;
 import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit.target.TestTarget;
@@ -16,18 +9,24 @@ import au.com.dius.pact.provider.spring.target.MockMvcTarget;
 import example.person.Person;
 import example.person.PersonRepository;
 import example.weather.WeatherClient;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 
-/** Teste de contrato
- * 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
  *  A provider test for the contract between this service (as a provider) and
  *  a primitive consumer. The implementation of the consumer can be
  *  found under https://github.com/hamvocke/spring-testing-consumer
  */
 
-
+@RunWith(RestPactRunner.class)
 @Provider("person_provider")// same as in the "provider_name" part in our pact file
 @PactFolder("target/pacts") // tells pact where to load the pact files from
-@SpringBootTest
 public class ExampleProviderTest {
 
     @Mock
@@ -41,8 +40,9 @@ public class ExampleProviderTest {
     @TestTarget
     public final MockMvcTarget target = new MockMvcTarget();
 
-    @BeforeEach
+    @Before
     public void before() {
+        initMocks(this);
         exampleController = new ExampleController(personRepository, weatherClient);
         target.setControllers(exampleController);
     }
